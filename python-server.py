c839cc1155a840fdf8951d644de41df72dd9219c
jordan-grindrod <JLGrindrod@hotmail.com>
2019-08-19 12:25:39 +0100

    added branching exercise

---
+++ python-server.py
@@ -0,0 +1,8 @@
+import http.server
+import socketserver
+
+PORT = 9000
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
